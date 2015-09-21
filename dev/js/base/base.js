b2c56d39b27bd8f0b8a3305499e86206115f0c27
丿赵文杰丶 <wenjie.zhao@dcfservice.net>
2015-09-21 14:00:50 +0800

    MD5加密

---
+++ dev/js/base/base.js
@@ -0,0 +1,15 @@
+/*
+ * base JS
+ *
+ * @author wenjie
+ * @since 07.20.2015
+ * @version 1.0.0
+ * @lastUpdatedBy Zhao Wenjie at 07.20.2015
+ 定义全局变量
+ */
+function getURL(){
+	return window.location.href;
+};
+var PATH = {
+	URL:getURL()
+};
