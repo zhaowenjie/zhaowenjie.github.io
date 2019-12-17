d6166aa715e9f4b3a90ca72a4586c3ada0243227
赵文杰 <zhaowenjie@surongtec.com>
2019-12-17 12:04:43 +0800

    u

---
+++ mobile/dai/precache-manifest.176bccfafa054f516d6d196bee20a5f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8384ab899b6729e9bd374387b0e79b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cac81f7757fe56805c",
+    "url": "/static/css/main.eae635f5.chunk.css"
+  },
+  {
+    "revision": "adf607ed83775399adac",
+    "url": "/static/js/2.01cb1624.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.01cb1624.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6cac81f7757fe56805c",
+    "url": "/static/js/main.d7377a46.chunk.js"
+  },
+  {
+    "revision": "ac2c2c8851482d6a317f",
+    "url": "/static/js/runtime-main.a2c0f2c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
